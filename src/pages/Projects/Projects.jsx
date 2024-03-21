c11ecbe7243d050cfd51a4d1a86409f6036794b0
Criss9005 <cdjpn5@gmail.com>
2024-03-20 22:06:06 -0500

    orden

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -12,56 +12,56 @@ import blog from '../../images/blog.jpg'
 
 const info = [
    {
-    id: 7,
+    id: 1,
     url: 'https://criss9005.github.io/filmoteka/',
     description: 'Buscador de peliculas, opciones de agregar a la libreria watched and queue movies, con ventana modal de la informacion de cada pelicula.',
     img: filmoteka,
     name:'Filmoteka'
   },
   {
-    id: 1,
+    id: 2,
     url: 'https://criss9005.github.io/goit-react-hw-08-phonebook/',
     description: 'Registro e inicio de sesion de Usuarios para tener una libreta de contactos privada. /*Developing...*/',
     img: phone,
     name:'Phonebook'
   },
   {
-    id: 2,
+    id: 3,
     url: 'https://criss9005.github.io/goit-react-hw-05-movies',
     description: 'Aplicación de búsqueda y almacenamiento de peliculas trabajando con la API themoviedb.org',
     img: trending,
     name:'Movies'
   },
   {
-    id: 3,
+    id: 4,
     url: 'https://criss9005.github.io/goit-react-hw-04-image-finder/',
     description: 'Búsqueda y almacenamiento de imagenes trabajando con el servicio público de búsqueda de imágenes Pixabay, con modal incluido.',
     img: collage,
     name:'Image Finder'
   },
   {
-    id: 4,
+    id: 5,
     url: 'https://criss9005.github.io/goit-js-hw-11/',
     description: 'Búsqueda de imagenes Pixabay, con Simpleligthbox, implementacion de scroll infinito.',
     img: collage,
     name:'Image Finder / Infinite Scroll '
   },
   {
-    id: 4,
+    id: 6,
     url: 'https://criss9005.github.io/goit-js-hw-10/',
     description: 'Buscador de razas de gatos, descripcion y temperamento usando JavaScript.',
     img: cats,
     name:'Cat breed finder'
   },
   {
-    id: 5,
+    id: 7,
     url: 'https://criss9005.github.io/goit-js-hw-09/',
     description: 'Cambiador de color aleatorio, contador de cuenta regresiva y generador de promesas.',
     img: color,
     name:'JavaScript'
   },
   {
-    id: 6,
+    id: 8,
     url: 'https://criss9005.github.io/goit-markup-hw-08/',
     description: 'Front de pagina Web con diseño adaptable (Mobile first), Aplicando metodologia BEM, utilizacion de SVG y ventana Modal.',
     img: adaptable,
@@ -70,7 +70,7 @@ const info = [
 ,
  
    {
-    id: 8,
+    id: 9,
     url: 'https://cristian-perez-naizzir-1102833522-5.blogspot.com/',
     description: 'Primeros pasos realizando un Blog de tecnologia',
     img: blog,
